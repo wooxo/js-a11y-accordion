d528a04213fa8b178e059b8679ab59d9d3e15cce
unknown <acorre@PC-ACORRE.wooxo.fr>
2015-09-08 17:53:33 +0200

    up

--- assets/js/add-mod.js
+++ assets/js/add-mod.js
@@ -11,6 +11,31 @@ function Accordion(o) {
         _.w = o.target;
         _.o = o;
     }
+    _.btns = [];
+    _.panels = [];
+
+    _.animTimer = function () {
+        this.classList.remove('isAnimated');
+        this.setAttribute('aria-hidden', true);
+        this.removeEventListener(_.o.anim, _.animTimer, true);
+    };
+
+    _.animEvent = function () {
+        var e,
+            el = document.createElement('fakeelement'),
+            events = {
+            'animation': 'animationend',
+            'OAnimation': 'oAnimationEnd',
+            'MozAnimation': 'animationend',
+            'WebkitTransition': 'webkitAnimationEnd'
+        };
+
+        for (e in events) {
+            if (el.style[e] !== undefined) {
+                return events[e];
+            }
+        }
+    };
 
     _.isTab = function (el) {
         return el.hasAttribute('role') && el.getAttribute('role') === 'tab';
@@ -24,40 +49,37 @@ function Accordion(o) {
         e = e ? e : arguments[0];
 
         var t = e.target || e.srcElement,
-            kc = e.keyCode;
-
+            kc = e.keyCode,
+            em = e.currentTarget;
+        if (em == document && _.o.closeOut) {
+            _.closeAll();
+        }
         if (_.isTab(t) && (e.type === 'click' || e.type === 'keydown' && (kc === 13 || kc === 32))) {
 
-            if (_.o.single) _.closeAll(t);
-
-            _.toggle(t);
+            _.closeAll();
+            _.toggle(_.current(t));
         }
         if (e.type === "keydown") {
             if (e.ctrlKey === true) {
-                // if user press ctrl + up arrow
-                if (e.keyCode === 38) {
+                if (kc === 38) {
                     _.getTab(e).focus();
                 }
-
-                // if user press ctrl + page up
-                if (e.keyCode === 33) {
+                if (kc === 33) {
                     _.move2(_.current(_.getTab(e)) - 1);
                 }
-                // if user press ctrl + page down
-                if (e.keyCode === 34) {
+                if (kc === 34) {
                     _.move2(_.current(_.getTab(e)) + 1);
                 }
             }
             if (_.isTab(t)) {
-                if (e.keyCode === 37 || e.keyCode === 38) {
+                if (kc === 37 || kc === 38) {
                     _.move2(_.current(t) - 1);
                 }
-                if (e.keyCode === 39 || e.keyCode === 40) {
+                if (kc === 39 || kc === 40) {
                     _.move2(_.current(t) + 1);
                 }
-                // end
-                if (e.keyCode === 36) _.move2(0);
-                if (e.keyCode === 35) _.move2(_.btns.length - 1);
+                if (kc === 36) _.move2(0);
+                if (kc === 35) _.move2(_.btns.length - 1);
             }
         }
         e.stopPropagation();
@@ -65,16 +87,32 @@ function Accordion(o) {
 
     _.closeAll = function (t) {
         for (var i = 0; i < _.btns.length; i++) {
-            if (_.btns[i] !== t) _.close(i);
+
+            if (_.btns[i] !== t && _.o.single) _.close(i);
         }
     };
-
-    _.close = function (i) {
-        var b = _.btns[i],
-            p = _.panels[i];
-        b.setAttribute('aria-selected', false);
-        b.setAttribute('aria-expanded', false);
-        p.setAttribute('aria-hidden', true);
+    _.open = function (index) {
+        var btn = _.btns[index],
+            panel = _.panels[index];
+        if (btn.getAttribute('aria-expanded') === "false") {
+            btn.setAttribute('aria-selected', true);
+            btn.setAttribute('aria-expanded', true);
+            panel.setAttribute('aria-hidden', false);
+        }
+    };
+    _.close = function (index) {
+        var btn = _.btns[index],
+            panel = _.panels[index];
+        if (btn.getAttribute('aria-expanded') === "true") {
+            btn.setAttribute('aria-selected', false);
+            btn.setAttribute('aria-expanded', false);
+            if (_.o.anim) {
+                panel.classList.add('isAnimated');
+                panel.addEventListener(_.o.anim, _.animTimer, true);
+            } else {
+                panel.setAttribute('aria-hidden', true);
+            }
+        }
     };
 
     _.getTab = function (e) {
@@ -85,7 +123,6 @@ function Accordion(o) {
         while (!_.isTabPanel(cc)) {
             cc = cc.parentElement;
         }
-        console.log(_.w);
         return _.w.querySelector("#" + cc.getAttribute('aria-labelledby'));
     };
 
@@ -106,23 +143,32 @@ function Accordion(o) {
         _.btns[i].focus();
     };
 
-    _.toggle = function (t) {
-        var s = t.getAttribute('aria-expanded');
-        s = s == 'false' ? true : false;
-
-        t.setAttribute('aria-expanded', s);
-        t.setAttribute('aria-selected', s);
-        t.nextElementSibling.setAttribute('aria-hidden', !s);
+    _.toggle = function (i) {
+        var s = _.btns[i].getAttribute('aria-expanded');
+        if (s === 'false') {
+            _.open(i);
+        } else {
+            _.close(i);
+        }
     };
 
     _.setup = function (w) {
-        var p, b, i;
+        var p,
+            b,
+            i,
+            els = w.children;
+
+        document.addEventListener('click', _.handleUserEvent);
+        document.addEventListener('keydown', _.handleUserEvent);
 
         w.setAttribute('role', 'tablist');
+
         w.setAttribute('aria-multiselectable', true);
 
-        _.btns = w.querySelectorAll('[role=tab]');
-        _.panels = w.querySelectorAll('[role=tabpanel]');
+        for (i = 0; i < els.length; i++) {
+            if (_.isTab(els[i])) _.btns.push(els[i]);
+            if (_.isTabPanel(els[i])) _.panels.push(els[i]);
+        };
 
         if (_.btns.length !== _.panels.length) {
             throw 'accordion module : numbers of control tabs and panels dont match';
@@ -145,15 +191,21 @@ function Accordion(o) {
 
     (function () {
         _.o.open = _.o.open || false;
+        _.o.anim = _.o.anim ? _.animEvent() : false;
         _.setup(_.o.target);
     })();
 }
 
 function pageLoaded() {
-    new Accordion({
-        target: document.querySelector('[data-widget=accordion]'),
-        single: true
-    });
+    var widgets = document.querySelectorAll('[data-widget=accordion]');
+    for (var i = 0; i < widgets.length; i++) {
+        new Accordion({
+            target: widgets[i],
+            single: true,
+            closeOut: true,
+            anim: true
+        });
+    }
 };
 
 document.addEventListener('Load', pageLoaded());
--- public/add-mod.v.dev.js
+++ public/add-mod.v.dev.js
@@ -10,10 +10,14 @@ function Accordion(o) {
         _.id = addCounter++;
         _.w = o.target;
         _.o = o;
-    };
+    }
+    _.btns = [];
+    _.panels = [];
+
     _.isTab = function (el) {
         return el.hasAttribute('role') && el.getAttribute('role') === 'tab';
     };
+
     _.isTabPanel = function (el) {
         return el.hasAttribute('role') && el.getAttribute('role') === 'tabpanel';
     };
@@ -25,37 +29,49 @@ function Accordion(o) {
             kc = e.keyCode;
 
         if (_.isTab(t) && (e.type === 'click' || e.type === 'keydown' && (kc === 13 || kc === 32))) {
+
+            _.closeAll(t);
             _.toggle(t);
         }
         if (e.type === "keydown") {
             if (e.ctrlKey === true) {
-                // if user press ctrl + up arrow
-                if (e.keyCode === 38) {
+                if (kc === 38) {
                     _.getTab(e).focus();
                 }
-
-                // if user press ctrl + page up
-                if (e.keyCode === 33) {
+                if (kc === 33) {
                     _.move2(_.current(_.getTab(e)) - 1);
                 }
-                // if user press ctrl + page down
-                if (e.keyCode === 34) {
+                if (kc === 34) {
                     _.move2(_.current(_.getTab(e)) + 1);
                 }
             }
             if (_.isTab(t)) {
-                if (e.keyCode === 37 || e.keyCode === 38) {
+                if (kc === 37 || kc === 38) {
                     _.move2(_.current(t) - 1);
                 }
-                if (e.keyCode === 39 || e.keyCode === 40) {
+                if (kc === 39 || kc === 40) {
                     _.move2(_.current(t) + 1);
                 }
-                // end
-                if (e.keyCode === 36) _.move2(0);
-                if (e.keyCode === 35) _.move2(_.btns.length - 1);
+                if (kc === 36) _.move2(0);
+                if (kc === 35) _.move2(_.btns.length - 1);
             }
         }
         e.stopPropagation();
+    };
+
+    _.closeAll = function (t) {
+        for (var i = 0; i < _.btns.length; i++) {
+
+            if (_.btns[i] !== t && _.o.single) _.close(i);
+        }
+    };
+
+    _.close = function (index) {
+        var btn = _.btns[index],
+            panel = _.panels[index];
+        btn.setAttribute('aria-selected', false);
+        btn.setAttribute('aria-expanded', false);
+        panel.setAttribute('aria-hidden', true);
     };
 
     _.getTab = function (e) {
@@ -97,13 +113,20 @@ function Accordion(o) {
     };
 
     _.setup = function (w) {
-        var p, b, i;
+        var p,
+            b,
+            i,
+            els = w.children;
 
         w.setAttribute('role', 'tablist');
+
         w.setAttribute('aria-multiselectable', true);
-        console.log();
-        _.btns = w.querySelectorAll('[role=tab]');
-        _.panels = w.querySelectorAll('[role=tabpanel]');
+
+        for (i = 0; i < els.length; i++) {
+            if (_.isTab(els[i])) _.btns.push(els[i]);
+            if (_.isTabPanel(els[i])) _.panels.push(els[i]);
+        };
+
         if (_.btns.length !== _.panels.length) {
             throw 'accordion module : numbers of control tabs and panels dont match';
         } else {
@@ -130,9 +153,13 @@ function Accordion(o) {
 }
 
 function pageLoaded() {
-    new Accordion({
-        target: document.querySelector('[data-widget=accordion]')
-    });
+    var widgets = document.querySelectorAll('[data-widget=accordion]');
+    for (var i = 0; i < widgets.length; i++) {
+        new Accordion({
+            target: widgets[i],
+            single: true
+        });
+    }
 };
 
 document.addEventListener('Load', pageLoaded());
